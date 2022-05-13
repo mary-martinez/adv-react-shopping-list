fdc554dacfea071f1bbaa4df50d51f8194b4ecdb
Mary Martinez <mary.diana.martinez@gmail.com>
2022-05-13 16:35:36 -0700

    testing is working to test rendered list plus adding items

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App', () => {
-  it.skip('should load a header, an input, and an initial list of items', () => {
+  it('should load a header, an input, and an initial list of items', () => {
     render(<App />);
+    const header = screen.getByText(/welcome/i);
+    expect(header).toBeInTheDocument();
+
+    const input = screen.getByRole('textbox');
+    const addBtn = screen.getByLabelText('submit new item');
+    userEvent.type(input, 'apple');
+    userEvent.click(addBtn);
+
+    const appleEdit = screen.getByLabelText('button to edit apple');
+    expect(appleEdit).toBeInTheDocument();
+
+    userEvent.type(input, 'potatoes');
+    userEvent.click(addBtn);
+
+    const potatoDelete = screen.getByLabelText('button to delete potatoes');
+    expect(potatoDelete).toBeInTheDocument();
+
+    screen.debug();
   });
 });
