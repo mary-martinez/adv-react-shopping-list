8aa63d0a7c948923ed672a4a0210e77e42658c30
Mary Martinez <mary.diana.martinez@gmail.com>
2022-05-12 18:00:22 -0700

    item component

---
+++ src/components/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Item() {
+  
+  
+  return (
+    <>
+      <h3>item</h3>
+      <button>edit</button>
+      <button>delete</button>
+    </>
+  );
+}
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 export default function List() {
-  
-  return <div>List</div>;
+  return (
+    <>
+      <h3>Your List</h3>
+      <ul>
+        <li>Apples</li>
+      </ul>
+    </>
+  );
 }
 
 // need to use useReducer; start by rendering from state first?
